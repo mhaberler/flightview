f97ee2d2a91f14ebd7569ca93e27a977b14f6c9d
Balázs Dukai <balazs.dukai@gmail.com>
2023-01-04 06:53:25 +0100

    Zoom to tileset

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
-import { Viewer, Cesium3DTileset, createWorldTerrain, Cartesian3, Math } from "cesium";
+import {
+    Viewer, Cesium3DTileset, createWorldTerrain, Cartesian3, Math,
+    viewerCesium3DTilesInspectorMixin, HeadingPitchRange
+} from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "../src/css/main.css"
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
 const viewer = new Viewer('cesiumContainer', {
-  terrainProvider: createWorldTerrain()
+    terrainProvider: createWorldTerrain()
 });
+
+viewer.scene.globe.depthTestAgainstTerrain = true;
+
+viewer.extend(viewerCesium3DTilesInspectorMixin);
+const inspectorViewModel = viewer.cesium3DTilesInspector.viewModel;
 
 // 3dtiles.data is a service created by docker compose and /3dtiles is mounted there
 const tileset = viewer.scene.primitives.add(new Cesium3DTileset({
-    url : 'http://localhost:8003/3dtiles/tileset.json'
+    url: 'http://localhost:8003/3dtiles/tileset.json',
+    enableDebugWireframe: false,
+    debugShowBoundingVolume: true,
+    debugShowContentBoundingVolume: false,
 }));
 
 // Fly the camera to Delft.
-viewer.camera.flyTo({
-  destination: Cartesian3.fromDegrees(4.365306, 52.005689, 400),
-  orientation : {
-    heading : Math.toRadians(0.0),
-    pitch : Math.toRadians(-15.0),
-  }
+// viewer.camera.flyTo({
+//     destination: Cartesian3.fromDegrees(4.365306, 52.005689, 400),
+//     orientation: {
+//         heading: Math.toRadians(0.0),
+//         pitch: Math.toRadians(-15.0),
+//     }
+// });
+
+tileset.readyPromise.then(function () {
+  viewer.zoomTo(
+    tileset,
+    new HeadingPitchRange(
+      0.0,
+      -0.5,
+      400
+      // tileset.boundingSphere.radius / 4.0
+    )
+  );
 });
