dc3d1ddb8b45c4fd9e0ca05ca39d0e8d0c2958b7
Balázs Dukai <balazs.dukai@gmail.com>
2023-01-04 21:31:34 +0100

    Enable request render mode

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import "../src/css/main.css"
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
 const viewer = new Viewer('cesiumContainer', {
-    terrainProvider: createWorldTerrain()
+    terrainProvider: createWorldTerrain(),
+    // https://cesium.com/blog/2018/01/24/cesium-scene-rendering-performance/#enabling-request-render-mode
+    requestRenderMode : true
 });
 
 viewer.scene.globe.depthTestAgainstTerrain = true;
