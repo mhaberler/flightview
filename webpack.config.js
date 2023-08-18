2d2fdbccf77be066e02dfd80904af9c65de47c34
Michael Haberler <git@mah.priv.at>
2023-08-18 14:51:31 +0200

    polish build and deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,53 +6,61 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    context: __dirname,
-    entry: {
-        app: './src/index.js'
-    },
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'dist'),
-        sourcePrefix: ''
-    },
-    resolve: {
-        fallback: { "https": false, "zlib": false, "http": false, "url": false },
-        mainFiles: ['index', 'Cesium']
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }, {
-            test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-            use: ['url-loader']
+module.exports = (env) => {
+    // see https://webpack.js.org/guides/environment-variables/
+    let baseUrl = '';
+    if (env.baseUrl) {
+        baseUrl = env.baseUrl;      
+    }
+    console.log(`setting baseUrl to ${baseUrl}`);
+    return {
+        context: __dirname,
+        entry: {
+            app: './src/index.js'
         },
-        {
-            test: /\.json$/,
-            use: ['json-loader'],
-            type: 'javascript/auto',
+        output: {
+            filename: 'app.js',
+            path: path.resolve(__dirname, 'dist'),
+            sourcePrefix: ''
         },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
-        // Copy Cesium Assets, Widgets, and Workers to a static directory
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
-                { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
-                { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' },
-                { from: path.join(cesiumSource, 'ThirdParty'), to: 'ThirdParty' }
+        resolve: {
+            fallback: { "https": false, "zlib": false, "http": false, "url": false },
+            mainFiles: ['index', 'Cesium']
+        },
+        module: {
+            rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }, {
+                test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+                use: ['url-loader']
+            },
+            {
+                test: /\.json$/,
+                use: ['json-loader'],
+                type: 'javascript/auto',
+            },
             ]
-        }),
-        new webpack.DefinePlugin({
-            // Define relative base path in cesium for loading assets
-            CESIUM_BASE_URL: JSON.stringify('')
-        })
-    ],
-    mode: 'development',
-    devtool: 'eval',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'src/index.html'
+            }),
+            // Copy Cesium Assets, Widgets, and Workers to a static directory
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
+                    { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
+                    { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' },
+                    { from: path.join(cesiumSource, 'ThirdParty'), to: 'ThirdParty' }
+                ]
+            }),
+            new webpack.DefinePlugin({
+                // Define relative base path in cesium for loading assets
+                CESIUM_BASE_URL: JSON.stringify(baseUrl)
+            })
+        ],
+        mode: 'development',
+        devtool: 'eval',
+    }
 };
